c77a409702e4a6ef6232fa1b3a0ced4a956490c8
vector <asliddin.forwork@gmail.com>
2026-06-30 13:15:50 +0500

    update: migrate.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ echo "Waiting for postgres"
 sleep 10
 
 echo "Running migrations"
-uv run alembic upgrade head
+# uv run alembic upgrade head
+uv run migrate.py
 
 echo "Starting server"
 exec uv run uvicorn src.app:app --host 0.0.0.0 --port 8000
