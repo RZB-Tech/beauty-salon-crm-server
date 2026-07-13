cd7942ccd9a8a49860c5a3254f9d84a277eff0ca
vector <asliddin.forwork@gmail.com>
2026-07-13 20:34:33 +0500

    update: require lastname and age verification employee create

--- src/schemas/employee/create.py
+++ src/schemas/employee/create.py
@@ -1,9 +1,10 @@
 from datetime import date
-from pydantic import BaseModel, ConfigDict, Field
+from fastapi import HTTPException
+from pydantic import BaseModel, Field, field_validator
 
 class EmployeeCreateSchema(BaseModel):
-    firstname: str = Field(..., max_length=100, description="Employee's first name")
-    lastname: str | None = Field(None, max_length=100)
+    firstname: str = Field(max_length=100, description="Employee's first name")
+    lastname: str = Field(max_length=100)
     middlename: str | None = Field(None, max_length=100)
     phone: str | None = Field(None, max_length=50)
     birth_date: date
@@ -13,3 +14,19 @@ class EmployeeCreateSchema(BaseModel):
     salary_fixed: int | None = 0
     percent_from_services: int | None = 0
     percent_from_sales: int | None = 0
+
+    @field_validator("birth_date")
+    @classmethod
+    def validate_age(cls, birth_date: date) -> date:
+        today = date.today()
+
+        age = (
+            today.year
+            - birth_date.year
+            - ((today.month, today.day) < (birth_date.month, birth_date.day))
+        )
+
+        if age < 18:
+            raise HTTPException(400, "Возраст сотрудника должен быть не менее 18 лет.")
+
+        return birth_date
