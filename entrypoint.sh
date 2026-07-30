fa78630388bd5dc86250c47b5914f311c99098bd
laconal <asliddin.forwork@gmail.com>
2026-07-30 20:29:17 +0500

    microfix

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,8 +32,5 @@ fi
 echo "Waiting for postgres"
 sleep 10
 
-# echo "Running migrations"
-# uv run migrate_docker.py
-
 echo "Starting server"
 exec uv run uvicorn src.app:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips="*"
