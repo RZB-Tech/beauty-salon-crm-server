ad432f4291f52de56f71d2ca44194e47d4feefca
laconal <asliddin.forwork@gmail.com>
2026-09-17 01:10:57 +0500

    update: docker-compose and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,4 +35,4 @@ sleep 10
 uv run alembic upgrade head
 
 echo "Starting server"
-exec uv run uvicorn src.app:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips="*"
+exec uv run uvicorn src.app:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips="172.17.0.1"
